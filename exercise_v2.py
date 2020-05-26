4b75ee9732c6cae75f7d2d1a047d4566ced507b9
xtron <xtron@xtrons-MacBook-Pro.local>
2020-05-26 09:52:39 +0200

    clean base

--- exercise_v2.py
+++ exercise_v2.py
@@ -14,25 +14,39 @@ from core import workprogram
 from core import keylistener
 
 
-#def background():
-#    workprogram.run( gamecontroller.exerciseEvent )
-#def foreground( arx ):
-#    keylistener.run( gamecontroller.completionEvent )
-#b = threading.Thread( target=background , daemon=True )
-#f = threading.Thread( target=foreground , daemon=True , args=(1,) )
 
-b = threading.Thread( 
-    target=workprogram.run , 
-    daemon=True , 
-    args=( gamecontroller.exerciseEvent,) )
-
-f = threading.Thread( 
-    target=keylistener.run , 
-    daemon=True , 
-    args=( gamecontroller.completionEvent,) )
+b = threading.Thread( target=workprogram.run , daemon=True , args=( gamecontroller.exerciseEvent,) )
+f = threading.Thread( target=keylistener.run , daemon=True , args=( gamecontroller.completionEvent,) )
 
 b.start()
 f.start()
 
 print('Starting Supervillain..')
 gamecontroller.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def background():
+#    workprogram.run( gamecontroller.exerciseEvent )
+#def foreground( arx ):
+#    keylistener.run( gamecontroller.completionEvent )
+#b = threading.Thread( target=background , daemon=True )
+#f = threading.Thread( target=foreground , daemon=True , args=(1,) )
--- exp/topfarmer.py
+++ exp/topfarmer.py
@@ -6,15 +6,20 @@ import os
 first_names = ['john','ron','burt','bob','garry', 'moe','tom','frank','chris','will','johnny','nick']
 last_names = ['smith','johnson','thompson','williamson','mcdonald','brown','ericson']
 #first_names = ['apple','moon','star','summer','rain','lilly','rosemarry','basil']
-first_names = ['ap','bo','ja','al','za']
-last_names = ['dex','jax','dax','vue']
+#first_names = ['ap','bo','ja','al','za']
+#last_names = ['dex','jax','dax','vue']
 
 all_names = []
 for f in first_names:
     for l in last_names:
-        print(f+''+l)
+        all_names.append(f+' '+l)
+        print(f+' '+l)
         
 
 import random
-while False:
-    os.system('say '+ all_names[ int(random.random()*79) ]  )
+iter = 0
+while True and iter < 100:
+    curname = all_names[ int(random.random()* len( all_names)-1 ) ]
+    os.system('say '+ curname  )
+    iter += 1
+    print( iter, curname )
