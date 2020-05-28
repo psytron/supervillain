8a063bb88e3e3bbc8538a264454df3bef1d474ce
xtron <xtron@xtrons-MacBook-Pro.local>
2020-05-28 02:43:46 +0200

    wip

--- exp/fibonaci.py
+++ exp/fibonaci.py
@@ -4,14 +4,17 @@ import time,sys
 iter=0
 x,y,z=1,1,0
 starttime = time.time()
+finalnum = 0 
 while True:
     z = x + y
     x = y
     y = z
-    print( iter,' : ',z )
+    finalnum = z
+    #print( iter,' : ',z )
     #time.sleep(0.01 )
     iter+=1
-    if iter > 50000: 
+    if iter > 1000000: 
+        print( finalnum )
         print( time.time()- starttime)
         sys.exit()
 
